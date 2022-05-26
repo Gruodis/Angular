25378d18b171db022565920ab67ee6aea0f2c3b4
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-05-26 23:21:32 +0300

    generic

--- class_generic/app.js
+++ class_generic/app.js
@@ -1,26 +1,18 @@
+"use strict";
 // parametrizuotos(generic) funkcijos
-
 let laipsniai = [5, 6, 7, 8, 9, 10];
 let vardai = ['Jonas', 'Petras', 'Bronius'];
-
-function pridetiLaipsni<T>(masyvas: T[], reiksme: T) {
+function pridetiLaipsni(masyvas, reiksme) {
     const naujasMasyvas = [reiksme, ...masyvas];
     return naujasMasyvas;
 }
-
-
 laipsniai = pridetiLaipsni(laipsniai, 1);
 laipsniai = pridetiLaipsni(laipsniai, 3);
 laipsniai = pridetiLaipsni(laipsniai, 4);
-
-
-vardai = pridetiLaipsni<string>(vardai, 'Gedas');
-
+vardai = pridetiLaipsni(vardai, 'Gedas');
 // sort funkcija rasome po pridetiLaipsni
 laipsniai.sort(function (a, b) {
     return a - b;
 });
-
-
 console.log(laipsniai);
 console.log(vardai.sort());
