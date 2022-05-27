4e8b7ef5d5e41a4a86ca49c6b33865d56ed18e42
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-05-27 13:54:07 +0300

    update generic fun

--- class_generic/app.js
+++ class_generic/app.js
@@ -1,5 +1,23 @@
 "use strict";
 // parametrizuotos(generic) funkcijos
+// generic arrow function, kuri:
+let lastItem = (x) => {
+    return x[x.length - x.length]; //grazins pirma reiksme masyve
+    // return x[x.length - 1] // grazins paskutine reiksme masyve
+};
+let l = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(l, lastItem(l));
+let l1 = ['Jonas', 'Antanas', 'Gintas'];
+console.log(lastItem(l1) // iskviedami funkcija galime nurodyti koks duomenu tipas patenka i funkcija naudodami <string> 
+);
+// generic arrow function, kuri:
+let sujungiam = (x, y) => {
+    return [x, y]; //grazins masyva sudaryta is reiksmiu, kurias nurodysime iskviesdami funkcija
+};
+let l2 = sujungiam('Simas', 9); // galime nenurodyti argumentu('simas', 9) tipo, TypeScript priskirs tipus savarankiskai
+let l3 = sujungiam('', 9); // galime nurodyti konkretu argumento tipa, kuris gali buti string arba null
+console.log('L2', l2, sujungiam(9, 'Gedas'));
+////////////////////////////////////////////////////////////////
 let laipsniai = [5, 6, 7, 8, 9, 10];
 let vardai = ['Jonas', 'Petras', 'Bronius'];
 function pridetiLaipsni(masyvas, reiksme) {
--- class_generic/app.ts
+++ class_generic/app.ts
@@ -1,5 +1,35 @@
 // parametrizuotos(generic) funkcijos
 
+
+
+// generic arrow function, kuri:
+let lastItem = <T>(x: T[]) => {
+    return x[x.length - x.length] //grazins pirma reiksme masyve
+    // return x[x.length - 1] // grazins paskutine reiksme masyve
+}
+let l = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(l, lastItem(l));
+
+let l1 = ['Jonas', 'Antanas', 'Gintas'];
+console.log(lastItem<string>(l1) // iskviedami funkcija galime nurodyti koks duomenu tipas patenka i funkcija naudodami <string> 
+);
+
+
+
+
+
+// generic arrow function, kuri:
+let sujungiam = <T, Y>(x: T, y: Y): [T, Y] => { // funkcija kurios tipas <T,Y> yra generic, su dviem parametrais (x:T,y:Y), grazina masyva :[T,Y]
+    return [x, y] //grazins masyva sudaryta is reiksmiu, kurias nurodysime iskviesdami funkcija
+}
+
+let l2 = sujungiam('Simas', 9); // galime nenurodyti argumentu('simas', 9) tipo, TypeScript priskirs tipus savarankiskai
+let l3 = sujungiam<string | null, number>('', 9); // galime nurodyti konkretu argumento tipa, kuris gali buti string arba null
+console.log('L2', l2, sujungiam(9, 'Gedas'));
+
+
+////////////////////////////////////////////////////////////////
+
 let laipsniai = [5, 6, 7, 8, 9, 10];
 let vardai = ['Jonas', 'Petras', 'Bronius'];
 
